bf39c3040d5fe8197e07056c452e672adfc3caea
sushil kumar <sushil.kumar@infinte.com>
2023-07-23 09:16:52 +0530

    minor

--- index.js
+++ index.js
@@ -2,99 +2,128 @@ const PORT = process.env.PORT || 8000
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio');
-
-
-const searchString = "finance";
-const encodedString = encodeURI(searchString);
-
-
-
-// /import './module-name'////
-// const gnews = require('gnews-scraper');
-
+var mysql = require('mysql');
 const app = express();
+// const mysql = msq()
+
+
+const hostname = "localhost",
+    username = "root",
+    password = "12345678",
+    databsename = "BankDatabase"
+
+
+// Establish connection to the database
+// let con =  mysql.createConnection({
+//     host: hostname,
+//     user: username,
+//     port :PORT,
+//     password: password,
+//     database: databsename,
+// });
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "12345678",
+    database: "BankDatabase"
+});
+
+
+con.connect(function (err) {
+    console.log("Try to connect")
+    if (err) {
+        console.log("Error ", err)
+        throw err
+    };
+    console.log("Connected");
+    let databaseName = "BankDatabase";
+    let createQuery = `CREATE DATABASE IF NOT EXISTS ${databaseName}`;
+    con.query(createQuery, (err) => {
+        if (err) throw err;
+
+        console.log("Database Created Successfully !");
+
+
+    })
+    // con.query("DROP TABLE BankInfo", (err, drop) => {
+    // if (err)
+    //     console.log("ERROR: ", err);
+    // });
+  
+    var createStatament =
+        "CREATE TABLE IF NOT EXISTS BankInfo (id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(255),icon VARCHAR(1000),mobileBalance VARCHAR(255),callRequiredForbalance boolean DEFAULT false,smsTemplate VARCHAR(255),mobileMiniStatement VARCHAR(500),type VARCHAR(255),isPopular boolean DEFAULT false,CallRequiredForMiniStatement boolean DEFAULT false,customerCareNumber VARCHAR(255),twitter VARCHAR(255),email VARCHAR(255),headQuarters VARCHAR(255),founded VARCHAR(255),website VARCHAR(500),code VARCHAR(500),netbanking VARCHAR(1000),fdrates VARCHAR(1000))"
+
+    // Creating table "sample"
+    con.query(createStatament, (err, drop) => {
+        if (err)
+            console.log("ERROR: ", err);
+    });
+    //
+
+
+
+
+});
+
+
+
+
+
+const fileName = "IFSC_2021.csv";
+
+// csv().fromFile(fileName).then(source => {
+
+//   console.log(
+//     "Runing");
+//     // Fetching the data from each row 
+//     // and inserting to the table "sample"
+//     for (var i = 0; i < source.length; i++) {
+//           var BANK = source[i]["BANK"],
+//             IFSC = source[i]["IFSC"],
+//             BRANCH = source[i]["BRANCH"],
+//             CENTRE = source[i]["CENTRE"],
+//             DISTRICT = source[i]["DISTRICT"],
+//             STATE = source[i]["STATE"],
+//             ADDRESS = source[i]["ADDRESS"],
+//             CONTACT = source[i]["CONTACT"],
+//             IMPS = source[i]["IMPS"],
+//             RTGS = source[i]["RTGS"],
+//             CITY = source[i]["CITY"],
+//             ISO3166 = source[i]["ISO3166"],
+//             NEFT = source[i]["NEFT"],
+//             MICR = source[i]["MICR"],
+//             UPI = source[i]["UPI"],
+//             SWIFT = source[i]["SWIFT"]
+
+
+//         var insertStatement = 
+//         `INSERT INTO IFSC_DATA values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+//         var items = [BANK,IFSC,BRANCH,CENTRE,DISTRICT,STATE,ADDRESS,CONTACT,IMPS,RTGS,CITY,ISO3166,NEFT,MICR,UPI,SWIFT];
+
+//         // Inserting data of current row
+//         // into database
+//     //     con.query(insertStatement, items, 
+//     //         (err, results, fields) => {
+//     //         if (err) {
+//     //             console.log(
+//     // "Unable to insert item at row ", i + 1);
+//     //             return console.log(err);
+//     //         }
+//     //     });
+//     }
+//     console.log(
+// "All items stored into database successfully");
+// });
+
 
 //Url For Web-Scrapping
 const petrol_urls = {
     "BankHoliday_url": "https://www.bankbazaar.com/indian-holiday/bank-holidays.html",
     "Petrol_url": "https://www.goodreturns.in/petrol-price.html",
     "Diesel_url": "https://www.goodreturns.in/diesel-price.html",
-    "AN_url": "https://www.goodreturns.in/petrol-price-in-andaman-nicobar-s1.html",
-    "AP_url": "https://www.goodreturns.in/petrol-price-in-andhra-pradesh-s2.html",
-    "AR_url": "https://www.goodreturns.in/petrol-price-in-arunachal-pradesh-s3.html",
-    "AS_url": "https://www.goodreturns.in/petrol-price-in-assam-s4.html",
-    "BR_url": "https://www.goodreturns.in/petrol-price-in-bihar-s5.html",
-    "CH_url": "https://www.goodreturns.in/petrol-price-in-chandigarh-s6.html",
-    "CG_url": "https://www.goodreturns.in/petrol-price-in-chhatisgarh-s7.html",
-    "DH_url": "https://www.goodreturns.in/petrol-price-in-dadra-nagarhaveli-s8.html",
-    "DD_url": "https://www.goodreturns.in/petrol-price-in-daman-diu-s9.html",
-    "DL_url": "https://www.goodreturns.in/petrol-price-in-delhi-s10.html",
-    "GA_url": "https://www.goodreturns.in/petrol-price-in-goa-s11.html",
-    "GJ_url": "https://www.goodreturns.in/petrol-price-in-gujarat-s12.html",
-    "HR_url": "https://www.goodreturns.in/petrol-price-in-haryana-s13.html",
-    "HP_url": "https://www.goodreturns.in/petrol-price-in-himachal-pradesh-s14.html",
-    "JK_url": "https://www.goodreturns.in/petrol-price-in-jammu-kashmir-s15.html",
-    "JH_url": "https://www.goodreturns.in/petrol-price-in-jharkhand-s16.html",
-    "KA_url": "https://www.goodreturns.in/petrol-price-in-karnataka-s17.html",
-    "KL_url": "https://www.goodreturns.in/petrol-price-in-kerala-s18.html",
-    "MP_url": "https://www.goodreturns.in/petrol-price-in-madhya-pradesh-s19.html",
-    "MH_url": "https://www.goodreturns.in/petrol-price-in-maharashtra-s20.html",
-    "MN_url": "https://www.goodreturns.in/petrol-price-in-manipur-s21.html",
-    "ML_url": "https://www.goodreturns.in/petrol-price-in-meghalaya-s22.html",
-    "MZ_url": "https://www.goodreturns.in/petrol-price-in-mizoram-s23.html",
-    "NL_url": "https://www.goodreturns.in/petrol-price-in-nagaland-s24.html",
-    "OR_url": "https://www.goodreturns.in/petrol-price-in-odisha-s25.html",
-    "PY_url": "https://www.goodreturns.in/petrol-price-in-pondicherry-s26.html",
-    "PB_url": "https://www.goodreturns.in/petrol-price-in-punjab-s27.html",
-    "RJ_url": "https://www.goodreturns.in/petrol-price-in-rajasthan-s28.html",
-    "SK_url": "https://www.goodreturns.in/petrol-price-in-sikkim-s29.html",
-    "TN_url": "https://www.goodreturns.in/petrol-price-in-tamil-nadu-s30.html",
-    "TS_url": "https://www.goodreturns.in/petrol-price-in-telangana-s31.html",
-    "TR_url": "https://www.goodreturns.in/petrol-price-in-tripura-s32.html",
-    "UP_url": "https://www.goodreturns.in/petrol-price-in-uttar-pradesh-s33.html",
-    "UK_url": "https://www.goodreturns.in/petrol-price-in-uttarakhand-s34.html",
-    "WB_url": "https://www.goodreturns.in/petrol-price-in-west-bengal-s35.html"
-
 }
 
-const lpg_urls = {
-    "url": "https://www.goodreturns.in/lpg-price.html",
-    "AN_url": "https://www.goodreturns.in/lpg-price-in-andaman-nicobar-s1.html",
-    "AP_url": "https://www.goodreturns.in/lpg-price-in-andhra-pradesh-s2.html",
-    "AR_url": "https://www.goodreturns.in/lpg-price-in-arunachal-pradesh-s3.html",
-    "AS_url": "https://www.goodreturns.in/lpg-price-in-assam-s4.html",
-    "BR_url": "https://www.goodreturns.in/lpg-price-in-bihar-s5.html",
-    "CH_url": "https://www.goodreturns.in/lpg-price-in-chandigarh-s6.html",
-    "CG_url": "https://www.goodreturns.in/lpg-price-in-chhatisgarh-s7.html",
-    "DH_url": "https://www.goodreturns.in/lpg-price-in-dadra-nagarhaveli-s8.html",
-    "DD_url": "https://www.goodreturns.in/lpg-price-in-daman-diu-s9.html",
-    "DL_url": "https://www.goodreturns.in/lpg-price-in-delhi-s10.html",
-    "GA_url": "https://www.goodreturns.in/lpg-price-in-goa-s11.html",
-    "GJ_url": "https://www.goodreturns.in/lpg-price-in-gujarat-s12.html",
-    "HR_url": "https://www.goodreturns.in/lpg-price-in-haryana-s13.html",
-    "HP_url": "https://www.goodreturns.in/lpg-price-in-himachal-pradesh-s14.html",
-    "JK_url": "https://www.goodreturns.in/lpg-price-in-jammu-kashmir-s15.html",
-    "JH_url": "https://www.goodreturns.in/lpg-price-in-jharkhand-s16.html",
-    "KA_url": "https://www.goodreturns.in/lpg-price-in-karnataka-s17.html",
-    "KL_url": "https://www.goodreturns.in/lpg-price-in-kerala-s18.html",
-    "MP_url": "https://www.goodreturns.in/lpg-price-in-madhya-pradesh-s19.html",
-    "MH_url": "https://www.goodreturns.in/lpg-price-in-maharashtra-s20.html",
-    "MN_url": "https://www.goodreturns.in/lpg-price-in-manipur-s21.html",
-    "ML_url": "https://www.goodreturns.in/lpg-price-in-meghalaya-s22.html",
-    "MZ_url": "https://www.goodreturns.in/lpg-price-in-mizoram-s23.html",
-    "NL_url": "https://www.goodreturns.in/lpg-price-in-nagaland-s24.html",
-    "OR_url": "https://www.goodreturns.in/lpg-price-in-odisha-s25.html",
-    "PY_url": "https://www.goodreturns.in/lpg-price-in-pondicherry-s26.html",
-    "PB_url": "https://www.goodreturns.in/lpg-price-in-punjab-s27.html",
-    "RJ_url": "https://www.goodreturns.in/lpg-price-in-rajasthan-s28.html",
-    "SK_url": "https://www.goodreturns.in/lpg-price-in-sikkim-s29.html",
-    "TN_url": "https://www.goodreturns.in/lpg-price-in-tamil-nadu-s30.html",
-    "TS_url": "https://www.goodreturns.in/lpg-price-in-telangana-s31.html",
-    "TR_url": "https://www.goodreturns.in/lpg-price-in-tripura-s32.html",
-    "UP_url": "https://www.goodreturns.in/lpg-price-in-uttar-pradesh-s33.html",
-    "UK_url": "https://www.goodreturns.in/lpg-price-in-uttarakhand-s34.html",
-    "WB_url": "https://www.goodreturns.in/lpg-price-in-west-bengal-s35.html"
-}
+
 //
 const parsed_data = []
 parsed_data.petrol = []
@@ -227,46 +256,103 @@ function fetcher() {
         console.log(err)
     })
 }
-function fetcherBankHolidays() {
-    axios.get(petrol_urls["BankHoliday_url"]).then((response) => {
 
-        const html = response.data
-        const $ = cheerio.load(html)
-        //  console.log(html)
-        $("body > table > tbody > tr", html).each((index, element) => {
-            console.log($(element).find("td"));
-        });
+var bankInfo = {
+    id: "",
+    name: "",
+    icon: "",
+    mobileBalance: "",
+    callRequiredForbalance: false,
+    smsTemplate: "",
+    mobileMiniStatement: "",
+    type: "",
+    isPopular: false,
+    CallRequiredForMiniStatement: false,
+    customerCareNumber: "",
+    twitter: "",
+    email: "",
+    headQuarters: "",
+    founded: "",
+    website: "",
+    code: "",
+}
+app.get('/getbankdetails/:id', (req, res) => {
+    var query = `select * from BankInfo where id=${req.params.id}`
+    con.query(query, function (err, result, fields) {
+        if (err) throw err;
+            console.log(result);
+            res.send({ data: result })
+      });
 
-        // $('.owspan="1" colspan="1" class="highlight"', html).each(function () {
-        //     const capital = $(this).find('td:nth-child(1)').text();
-        //     const t_Price = $(this).find('td:nth-child(2)').text().replace(/\t|\n/gm, "");
-        //     const y_Price = $(this).find('td:nth-child(3)').text().replace(/\t|\n/gm, "");
+})
 
-        //     if(type === "petrol") 
-        //     {
-        //         parsed_data.petrol.push({
-        //             "City": capital,
-        //             "Today": t_Price,
-        //             "Yesterday": y_Price
-        //         })    
-        //     }
+app.get('/getbanklist', (req, res) => {
+    con.query("select id,name,icon,code from BankInfo", function (err, result, fields) {
+        if (err) throw err;
+            console.log(result);
+            res.send({ data: result })
+      });
 
-        // })
+})
+//sd
 
-        console.log(parsed_data.holidays)
+app.get('/push', (req, res) => {
+//
+    var firebase_url = "https://bank-info-ce9ad-default-rtdb.firebaseio.com/data.json"
+    axios.get(firebase_url).then((response) => {
+        // console.log(response.data)
+        var bankInfoArray = response.data;
 
+        for (var i in bankInfoArray) {
+            console.log(bankInfoArray[i]);
+            var name = bankInfoArray[i].name,
+            icon = bankInfoArray[i].icon,
+            mobileBalance = bankInfoArray[i].mobileBalance,
+            callRequiredForbalance = bankInfoArray[i].callRequiredForbalance,
+            smsTemplate = bankInfoArray[i].smsTemplate,
+            mobileMiniStatement = bankInfoArray[i].mobileMiniStatement,
+            type =  bankInfoArray[i].type,
+            isPopular = bankInfoArray[i].isPopular,
+            CallRequiredForMiniStatement = bankInfoArray[i].CallRequiredForMiniStatement,
+            customerCareNumber = bankInfoArray[i].customerCareNumber,
+            twitter  = bankInfoArray[i].twitter ,
+            email = bankInfoArray[i].email,
+            headQuarters = bankInfoArray[i].headQuarters,
+            founded = bankInfoArray[i].founded,
+            website = bankInfoArray[i].website,
+            code = bankInfoArray[i].code,
+            netbanking = "",
+            fdrates = ""
+             var insertStatement =
+            `INSERT INTO  BankInfo values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+             var items = [null,name, icon, mobileBalance, callRequiredForbalance, smsTemplate, mobileMiniStatement, type, isPopular,CallRequiredForMiniStatement,customerCareNumber, twitter, email, headQuarters, founded, website, code ,netbanking,fdrates];
+                        //  (null,'','','',true,'','','',false,false,'','','','','','','','','')
+             con.query(insertStatement, items, 
+                    (err, results, fields) => {
+                    if (err) {
+                        console.log(
+                       "Unable to insert item at row ", i + 1);
+                        return console.log(err);
+                    }
+                });
+
+        } 
+        console.log(
+            "All items stored into database successfully");
 
 
     }).catch(err => {
         // res.abort()
         console.log(err)
     })
-}
 
+
+
+})
 
 //Petrol Routers
 app.get('/', (req, res) => {
-    res.json("Hey Dev! Give '/<petrol/lpg>/<state-name>' with base Url to get respective Petrol Prices!!!");
+    res.json("Welcome Sir");
 })
 
 app.get('/fuel-price/india', (req, res) => {
@@ -276,12 +362,12 @@ app.get('/fuel-price/india', (req, res) => {
 app.get('/nsd', (req, res) => {
     console.log("APi Call")
     var time = istDate()
-    if(time!=nsData.time || nsData.news.length<200){
-            getNewsByApi(req,res)
-    }else {
+    if (time != nsData.time || nsData.news.length < 200) {
+        getNewsByApi(req, res)
+    } else {
         res.send(nsData.news)
     }
-    
+
 })
 
 
@@ -293,13 +379,13 @@ var dd_mm_yy = function () {
     return (curr_year + "-" + curr_month + "-" + curr_date);
 }
 
-var istDate  = function(){
-    var baseDate =  Date().toLocaleString('en-US', {timeZone: 'Asia/Kolkata'})
+var istDate = function () {
+    var baseDate = Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' })
     var d = new Date(baseDate)
     var curr_date = d.getDate();
-    if(curr_date<10) curr_date = `0${curr_date}`
+    if (curr_date < 10) curr_date = `0${curr_date}`
     var curr_month = d.getMonth() + 1;
-    if(curr_month<10) curr_month = `0${curr_month}`
+    if (curr_month < 10) curr_month = `0${curr_month}`
     var curr_year = d.getFullYear();
     return (curr_year + "-" + curr_month + "-" + curr_date);
 }
@@ -327,33 +413,19 @@ function intervalFunc() {
 }
 
 var preDate = 0
-const AXIOS_OPTIONS = {
-    headers: {
-        "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36",
-    },                                                  // adding the User-Agent header as one way to prevent the request from being blocked
-    params: {
-        q: encodedString,                                // our encoded search string        
-        tbm: "nws",                                     // parameter defines the type of search you want to do ("nws" means news)
-        hl: 'en',                                       // Parameter defines the language to use for the Google search
-        gl: 'in'                                        // parameter defines the country to use for the Google search
-    },
-};
 
-
-
-var nsData = { 
-    news: "", 
-    time:istDate()
- } 
-function getNewsByApi(req,res){
+var nsData = {
+    news: "",
+    time: istDate()
+}
+function getNewsByApi(req, res) {
     var business_url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=2fb087f1232f444a9f70f34855146fc6&category=business'
     // var business_url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=2fb087f1232f444a9f70f34855146fc6'
-     axios.get(business_url).then((response) => {
+    axios.get(business_url).then((response) => {
         console.log(response.data)
-         nsData.news = response.data;
-         nsData.time = istDate()
-         res.send(nsData.news)
+        nsData.news = response.data;
+        nsData.time = istDate()
+        res.send(nsData.news)
 
     }).catch(err => {
         // res.abort()
