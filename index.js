54326f882ffd18c455fb04fe4375254069e9dabb
sushil kumar <sushil.kumar@infinte.com>
2023-08-08 22:22:47 +0530

    minor

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ parsed_data.holidays = []
 
 //Main Script for parsing necessary data
 function script(res, url, type) {
-
+    console.log("Need to move ahed --> 11")
     var time = istDate()
 
     if (parsed_data.petrol.length > 0 && parsed_data.diesel.length > 0 && time == psData.time) {
@@ -166,65 +166,88 @@ function script(res, url, type) {
             diesel: parsed_data.diesel,
 
         };
+        console.log("Need to move ahed --> 2")
         res.json(data);
         return
     }
 
-    console.log("Need to move ahed")
+    console.log("Need to move ahed==>>,"+url)
+    // return
+    // axios.get(url).then(express.response)=>{
 
-    axios.get(url).then((response) => {
+    // }
 
-        const html = response.data
-        const $ = cheerio.load(html)
+    const params = {
+        headers: {
+            "User-Agent":
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+        },
+    };
+     // axios.config(config)
+ 
+      var url = 'https://www.goodreturns.in/petrol-price.html/*/'
+      axios.get(url, params).then(function (response) {
+       console.log('Respone---'+response);
+     
+    }).catch(err => {
+        console.log('ERROR OCCURED');
+        console.log(err)
+    })
+
+    // axios.get(url).then((response) => {
+
+    //     const html = response.data
+    //     const $ = cheerio.load(html)
+    //     console.log("---print $---",$)
 
 
-        $('.even_row, .odd_row', html).each(function () {
-            const capital = $(this).find('td:nth-child(1)').text();
-            const t_Price = $(this).find('td:nth-child(2)').text().replace(/\t|\n/gm, "");
-            const y_Price = $(this).find('td:nth-child(3)').text().replace(/\t|\n/gm, "");
+    //     // $('.even_row, .odd_row', html).each(function () {
+    //     //     const capital = $(this).find('td:nth-child(1)').text();
+    //     //     const t_Price = $(this).find('td:nth-child(2)').text().replace(/\t|\n/gm, "");
+    //     //     const y_Price = $(this).find('td:nth-child(3)').text().replace(/\t|\n/gm, "");
 
-            if (type === "petrol") {
-                parsed_data.petrol.push({
-                    "City": capital,
-                    "Today": t_Price,
-                    "Yesterday": y_Price
-                })
-            }
+    //     //     if (type === "petrol") {
+    //     //         parsed_data.petrol.push({
+    //     //             "City": capital,
+    //     //             "Today": t_Price,
+    //     //             "Yesterday": y_Price
+    //     //         })
+    //     //     }
 
-        })
-        axios.get(petrol_urls["Diesel_url"]).then((response) => {
-            const html = response.data
-            const $ = cheerio.load(html)
-
-
-
-            $('.even_row, .odd_row', html).each(function () {
-                const capital = $(this).find('td:nth-child(1)').text();
-                const t_Price = $(this).find('td:nth-child(2)').text().replace(/\t|\n/gm, "");
-                const y_Price = $(this).find('td:nth-child(3)').text().replace(/\t|\n/gm, "");
-                let tip = "diesel"
-                if (tip === "diesel") {
-                    parsed_data.diesel.push({
-                        "City": capital,
-                        "Today": t_Price,
-                        "Yesterday": y_Price
-                    })
-                }
-
-            })
-            var data = {
-
-                petrol: parsed_data.petrol,
-                diesel: parsed_data.diesel,
-
-            };
-            psData.time = istDate()
-            res.json(data);
-        }).catch(err => console.log(err))
+    //     // })
+    //     // axios.get(petrol_urls["Diesel_url"]).then((response) => {
+    //     //     const html = response.data
+    //     //     const $ = cheerio.load(html)
 
 
 
-    }).catch(err => console.log(err))
+    //     //     $('.even_row, .odd_row', html).each(function () {
+    //     //         const capital = $(this).find('td:nth-child(1)').text();
+    //     //         const t_Price = $(this).find('td:nth-child(2)').text().replace(/\t|\n/gm, "");
+    //     //         const y_Price = $(this).find('td:nth-child(3)').text().replace(/\t|\n/gm, "");
+    //     //         let tip = "diesel"
+    //     //         if (tip === "diesel") {
+    //     //             parsed_data.diesel.push({
+    //     //                 "City": capital,
+    //     //                 "Today": t_Price,
+    //     //                 "Yesterday": y_Price
+    //     //             })
+    //     //         }
+
+    //     //     })
+    //     //     var data = {
+
+    //     //         petrol: parsed_data.petrol,
+    //     //         diesel: parsed_data.diesel,
+
+    //     //     };
+    //     //     psData.time = istDate()
+    //     //     res.json(data);
+    //     // }).catch(err => console.log(err))
+
+
+
+    // }).catch(err => console.log(err))
 
 }
 // Petrol and Diesel Price
@@ -387,6 +410,13 @@ app.get('/gettopbanklist/:token', (req, res) => {
 
 
 })
+
+app.get("/price",(req,res)=>{
+
+
+
+
+});
 
 app.get('/getStateList/:bankcode/:token', (req, res) => {
     if(!isValidToken(req.params.token)){
@@ -641,16 +671,27 @@ app.get('/', (req, res) => {
     res.json("Welcome Sir");
 })
 
-app.get('/fuel-price/india/:token', (req, res) => {
-    if(!isValidToken(req.params.token)){
-        res.send("Invalid Token")
-        return
-    }
+// app.get('/fuel-price/india/:token', (req, res) => {
+//     // if(!isValidToken(req.params.token)){
+//     //     res.send("Invalid Token")
+//     //     return
+//     // }
+//     script(res, petrol_urls["Petrol_url"], "petrol");
+// })
+app.get('/fuel-price/india', (req, res) => {
+    // if(!isValidToken(req.params.token)){
+    //     res.send("Invalid Token")
+    //     return
+    // }
     script(res, petrol_urls["Petrol_url"], "petrol");
 })
 
+
 app.get('/privacy', (req, res) => {
     res.send(privacy_content)
+})
+app.get('/lte/privacy', (req, res) => {
+    res.send(privacy_content_lte)
 })
 
 app.get('/nsd/:token', (req, res) => {
@@ -778,17 +819,11 @@ function decryptData(data) {
 
 
 app.listen(PORT, () => {
-    setInterval(intervalFunc, 1500);
+    //setInterval(intervalFunc, 1500);
    
 
     var date = istDate()
-    // console.log("date---",date)
-    // console.log("encrpted---",encryptData(date))
-    // var encrpted = decryptData("2024-08-01")
-    // console.log("isValid---",isValidToken(encrpted))
 
-    // console.log("--decrpted",decryptData("F1eyRgoJ7YCRbpAiD1n1VS735xYj0O107LqMUTFPsaA="))
-    
     console.log(`Server  Sushil is running at PORT HELLO ${PORT}`)
 
 
@@ -798,6 +833,8 @@ app.listen(PORT, () => {
 
 
 })
+
+
 
 var privacy_content = "<!DOCTYPE html>\n" +
 "    <html>\n" +
@@ -827,6 +864,118 @@ var privacy_content = "<!DOCTYPE html>\n" +
 "                  The terms used in this Privacy Policy have the same meanings\n" +
 "                  as in our Terms and Conditions, which are accessible at\n" +
 "                  All Bank Balance unless otherwise defined in this Privacy Policy.\n" +
+"                </p> <p><strong>Information Collection and Use</strong></p> <p>\n" +
+"                  For a better experience, while using our Service, I\n" +
+"                  may require you to provide us with certain personally\n" +
+"                  identifiable information. The information that\n" +
+"                  I request will be retained on your device and is not collected by me in any way.\n" +
+"                </p> <div><p>\n" +
+"                    The app does use third-party services that may collect\n" +
+"                    information used to identify you.\n" +
+"                  </p> <p>\n" +
+"                    Link to the privacy policy of third-party service providers used\n" +
+"                    by the app\n" +
+"                  </p> <ul><li><a href=\"https://www.google.com/policies/privacy/\" target=\"_blank\" rel=\"noopener noreferrer\">Google Play Services</a></li><li><a href=\"https://support.google.com/admob/answer/6128543?hl=en\" target=\"_blank\" rel=\"noopener noreferrer\">AdMob</a></li><li><a href=\"https://firebase.google.com/policies/analytics\" target=\"_blank\" rel=\"noopener noreferrer\">Google Analytics for Firebase</a></li><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----></ul></div> <p><strong>Log Data</strong></p> <p>\n" +
+"                  I want to inform you that whenever you\n" +
+"                  use my Service, in a case of an error in the app\n" +
+"                  I collect data and information (through third-party\n" +
+"                  products) on your phone called Log Data. This Log Data may\n" +
+"                  include information such as your device Internet Protocol\n" +
+"                  (“IP”) address, device name, operating system version, the\n" +
+"                  configuration of the app when utilizing my Service,\n" +
+"                  the time and date of your use of the Service, and other\n" +
+"                  statistics.\n" +
+"                </p> <p><strong>Cookies</strong></p> <p>\n" +
+"                  Cookies are files with a small amount of data that are\n" +
+"                  commonly used as anonymous unique identifiers. These are sent\n" +
+"                  to your browser from the websites that you visit and are\n" +
+"                  stored on your device's internal memory.\n" +
+"                </p> <p>\n" +
+"                  This Service does not use these “cookies” explicitly. However,\n" +
+"                  the app may use third-party code and libraries that use\n" +
+"                  “cookies” to collect information and improve their services.\n" +
+"                  You have the option to either accept or refuse these cookies\n" +
+"                  and know when a cookie is being sent to your device. If you\n" +
+"                  choose to refuse our cookies, you may not be able to use some\n" +
+"                  portions of this Service.\n" +
+"                </p> <p><strong>Service Providers</strong></p> <p>\n" +
+"                  I may employ third-party companies and\n" +
+"                  individuals due to the following reasons:\n" +
+"                </p> <ul><li>To facilitate our Service;</li> <li>To provide the Service on our behalf;</li> <li>To perform Service-related services; or</li> <li>To assist us in analyzing how our Service is used.</li></ul> <p>\n" +
+"                  I want to inform users of this Service\n" +
+"                  that these third parties have access to their Personal\n" +
+"                  Information. The reason is to perform the tasks assigned to\n" +
+"                  them on our behalf. However, they are obligated not to\n" +
+"                  disclose or use the information for any other purpose.\n" +
+"                </p> <p><strong>Security</strong></p> <p>\n" +
+"                  I value your trust in providing us your\n" +
+"                  Personal Information, thus we are striving to use commercially\n" +
+"                  acceptable means of protecting it. But remember that no method\n" +
+"                  of transmission over the internet, or method of electronic\n" +
+"                  storage is 100% secure and reliable, and I cannot\n" +
+"                  guarantee its absolute security.\n" +
+"                </p> <p><strong>Links to Other Sites</strong></p> <p>\n" +
+"                  This Service may contain links to other sites. If you click on\n" +
+"                  a third-party link, you will be directed to that site. Note\n" +
+"                  that these external sites are not operated by me.\n" +
+"                  Therefore, I strongly advise you to review the\n" +
+"                  Privacy Policy of these websites. I have\n" +
+"                  no control over and assume no responsibility for the content,\n" +
+"                  privacy policies, or practices of any third-party sites or\n" +
+"                  services.\n" +
+"                </p> <p><strong>Children’s Privacy</strong></p> <div><p>\n" +
+"                    These Services do not address anyone under the age of 13.\n" +
+"                    I do not knowingly collect personally\n" +
+"                    identifiable information from children under 13 years of age. In the case\n" +
+"                    I discover that a child under 13 has provided\n" +
+"                    me with personal information, I immediately\n" +
+"                    delete this from our servers. If you are a parent or guardian\n" +
+"                    and you are aware that your child has provided us with\n" +
+"                    personal information, please contact me so that\n" +
+"                    I will be able to do the necessary actions.\n" +
+"                  </p></div> <!----> <p><strong>Changes to This Privacy Policy</strong></p> <p>\n" +
+"                  I may update our Privacy Policy from\n" +
+"                  time to time. Thus, you are advised to review this page\n" +
+"                  periodically for any changes. I will\n" +
+"                  notify you of any changes by posting the new Privacy Policy on\n" +
+"                  this page.\n" +
+"                </p> <p>This policy is effective as of 2023-08-01</p> <p><strong>Contact Us</strong></p> <p>\n" +
+"                  If you have any questions or suggestions about my\n" +
+"                  Privacy Policy, do not hesitate to contact me at sushil.sh2007@gmail.com.\n" +
+"                </p> <p>This privacy policy page was created at <a href=\"https://privacypolicytemplate.net\" target=\"_blank\" rel=\"noopener noreferrer\">privacypolicytemplate.net </a>and modified/generated by <a href=\"https://app-privacy-policy-generator.nisrulz.com/\" target=\"_blank\" rel=\"noopener noreferrer\">App Privacy Policy Generator</a></p>\n" +
+"    </body>\n" +
+"    </html>\n" +
+"      "
+
+
+var privacy_content_lte = "<!DOCTYPE html>\n" +
+"    <html>\n" +
+"    <head>\n" +
+"      <meta charset='utf-8'>\n" +
+"      <meta name='viewport' content='width=device-width'>\n" +
+"      <title>Privacy Policy</title>\n" +
+"      <style> body { font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; padding:1em; } </style>\n" +
+"    </head>\n" +
+"    <body>\n" +
+"    <strong>Privacy Policy</strong> <p>\n" +
+"                  IntellectX built the 5G/4G LTE SWITCHER app as\n" +
+"                  a Free app. This SERVICE is provided by\n" +
+"                  IntellectX at no cost and is intended for use as\n" +
+"                  is.\n" +
+"                </p> <p>\n" +
+"                  This page is used to inform visitors regarding my\n" +
+"                  policies with the collection, use, and disclosure of Personal\n" +
+"                  Information if anyone decided to use my Service.\n" +
+"                </p> <p>\n" +
+"                  If you choose to use my Service, then you agree to\n" +
+"                  the collection and use of information in relation to this\n" +
+"                  policy. The Personal Information that I collect is\n" +
+"                  used for providing and improving the Service. I will not use or share your information with\n" +
+"                  anyone except as described in this Privacy Policy.\n" +
+"                </p> <p>\n" +
+"                  The terms used in this Privacy Policy have the same meanings\n" +
+"                  as in our Terms and Conditions, which are accessible at\n" +
+"                  5G/4G LTE SWITCHER unless otherwise defined in this Privacy Policy.\n" +
 "                </p> <p><strong>Information Collection and Use</strong></p> <p>\n" +
 "                  For a better experience, while using our Service, I\n" +
 "                  may require you to provide us with certain personally\n" +
